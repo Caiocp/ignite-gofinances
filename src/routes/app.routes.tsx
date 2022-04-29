f290ac026c17f67a0044ab5412c2550a711e0f92
caiocp <caio.portela@fortbrasil.com.br>
2022-04-28 22:52:08 -0300

    corrige caminha das telas no arquivo de rota

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,11 +3,11 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useTheme } from 'styled-components';
 import { Platform } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import { AppRoutesParamList } from './@types/routes/app.routes';
+import { AppRoutesParamList } from '../@types/routes/app.routes';
 
-import { Dashboard } from './screens/Dashboard';
-import { Register } from './screens/Register';
-import { Resume } from './screens/Resume';
+import { Dashboard } from '../screens/Dashboard';
+import { Register } from '../screens/Register';
+import { Resume } from '../screens/Resume';
 
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutesParamList>();
 
